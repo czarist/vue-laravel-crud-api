7de3b05663ba216aa7c3e0e33e546c154e11934d
Cezar Trentin <55374787+czarist@users.noreply.github.com>
2023-04-19 21:05:20 -0300

    controllers e models

---
+++ database/migrations/2023_04_19_235217_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('category_id');
+            $table->text('detail');
+            $table->timestamps();
+
+            // Adicionando a chave estrangeira
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign(['category_id']);
+        });
+
+        Schema::dropIfExists('products');
+    }
+}
