c7c556c720d6fa4cf2fb5ff37b9a4934e767edf2
Cezar Trentin <55374787+czarist@users.noreply.github.com>
2023-04-19 23:06:42 -0300

    vue config

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,12 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" value="{{ csrf_token() }}" />
     <title>Vue JS CRUD Laravel</title>
-    <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet" />
+    <link href="{{ mix('/css/app.css') }}" type="text/css" rel="stylesheet" />
 </head>
 
 <body>
     <div id="app"></div>
-    <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
+    <script src="{{ mix('/js/app.js') }}" type="text/javascript"></script>
 </body>
 
 </html>
---
+++ webpack.mix.js
@@ -0,0 +1,7 @@
+const mix = require('laravel-mix');
+
+mix.js('resources/js/app.js', 'public/js')
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css')
+    .browserSync('localhost:8000')
+    .sourceMaps();
